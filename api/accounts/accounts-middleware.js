94a4de0fa9f1f656cf2758ecd171876167c70dd6
Drew Nelson <realseal177@gmail.com>
2022-02-14 21:23:39 -0800

    too late in night so i will have to turn in late

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,10 +1,12 @@
 const Account = require('./accounts-model');
 
 exports.checkAccountPayload = (req, res, next) => {
+  
   next()
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
+
   next()
 }
 
